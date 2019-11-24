42ec13c24bccafb729b35c71e7b9474a535a0305
ricou12 <>
2019-11-24 13:19:55 +0100

    affichage des miniatures sur full img js

---
+++ assets/js/vignette.js
@@ -0,0 +1,19 @@
+/** ************************************************************************************************************
+ *                                               S'EXCUTE AU CHARGEMENT DE LA PAGE
+ ************************************************************************************************************ */
+
+/** ***************************************************************************************
+ *                            ID DE  BALISE HTML
+ **************************************************************************************** */
+// Stocke dans des constantes, les ID de balise,
+// on pourra modifier, créer, supprimer des balises au niveau du fichier HTML via le DOM.
+const $fullImg = document.getElementById("fullImg");
+
+/** ************************************************************************************* */
+
+function showFullImg(){
+    // Récupere l'ensemble des attributs de la balise pour les stocker dans une variable objet.
+    var $vignette = event.target;
+    var path = $vignette.getAttribute("src");
+    $fullImg.setAttribute("src",path);
+}
