7e0eba45995263b0e45fb37f6c3c0d01d59bba9a
Ricou12 <dwwm2019@gmail.com>
2019-11-28 17:46:48 +0100

    modif

--- assets/js/vignette.js
+++ assets/js/vignette.js
@@ -5,6 +5,7 @@
 /** ***************************************************************************************
  *                            ID DE  BALISE HTML
  **************************************************************************************** */
+
 // Stocke dans des constantes, les ID de balise,
 // on pourra modifier, créer, supprimer des balises au niveau du fichier HTML via le DOM.
 const $fullImg = document.getElementById("fullImg");
