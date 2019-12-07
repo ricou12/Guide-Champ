b28e041b6057bfb90ed1f8f1acdac271c7442c48
ricou12 <>
2019-12-07 13:56:41 +0100

    Ajout application vb net parserJson

--- scripts/scriptRuby.rb
+++ scripts/scriptRuby.rb
@@ -1,11 +1,11 @@
-puts "Ouvre le fichier yaml" 
-file = File.open("_data/mushroom.yml", "r") 
-fichier = file 
+# puts "Ouvre le fichier yaml" 
+# file = File.open("_data/mushroom.yml", "r") 
+# fichier = file 
 
-#fichier = fichier.read 
-puts "split sur le caractere 'dossier_photo:'" 
-fichier = fichier.split("dossier_photo:") 
-fichier.each do |col|
-    puts "#{col}"
-end
-file.close
+# #fichier = fichier.read 
+# puts "split sur le caractere 'dossier_photo:'" 
+# fichier = fichier.split("dossier_photo:") 
+# fichier.each do |col|
+#     puts "#{col}"
+# end
+# file.close
